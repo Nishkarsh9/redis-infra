0f6ea7cbaf329c7a5e31f75bfc912ccd87fa2755
Nishkarsh9 <115409219+Nishkarsh9@users.noreply.github.com>
2025-03-10 17:16:51 +0530

    commiting redis infra assets

--- redis-terraform/variables.tf
+++ redis-terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1" 
+}
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
